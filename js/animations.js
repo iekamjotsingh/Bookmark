d627cd593b72d3074a706203be070653f7a68c10
Ekamjot Singh <68366451+iekamjotsingh@users.noreply.github.com>
2020-12-15 10:58:11 +0530

    Add files via upload

--- js/animations.js
+++ js/animations.js
@@ -26,59 +26,59 @@ $(document).ready(function () {
     $("#faq-" + this.id).slideToggle(500);
   });
 
-  function leftToRight(p1, delay) {
-    anime({
-      targets: [p1],
-      translateX: ["+100", "0"],
-      opacity: ["0%", "100%"],
-      easing: "easeInOutSine",
-      duration: 1000,
-      delay: delay,
-    });
-  }
-  $(".heading-first-ekam").on("inview", function (event, isInView) {
-    if (isInView) {
-      anime({
-        targets: [".heading-first-ekam", ".para-first-ekam"],
-        translateX: [-250, 0],
-        duration: 1000,
-        easing: "easeInOutSine",
-        opacity: ["0%", "100%"],
-        translateZ: 0,
-      });
-      leftToRight(".right-side-hero-img", 500);
-      anime({
-        targets: [".color-main"],
-        translateX: [+20, 0],
-        opacity: ["90%", "100%"],
-        easing: "easeInOutSine",
-        duration: 500,
-        translateZ: 0,
-      });
-    } else {
-      anime({
-        targets: [".heading-first-ekam", ".para-first-ekam"],
-        translateX: 0,
-        opacity: 0,
-      });
-      anime({
-        targets: [".right-side-hero-img"],
-        translateX: 0,
-        opacity: 0,
-      });
-    }
-  });
-  $(".color-features").on("inview", function (event, isInView) {
-    if (isInView) {
-      leftToRight(".heading-for-features-tab", 0);
-      anime({
-        targets: [".color-features"],
-        translateX: [-300, 0],
-        opacity: ["100%"],
-        easing: "easeInOutSine",
-        duration: 500,
-        translateZ: 0,
-      });
-    }
-  });
+  // function leftToRight(p1, delay) {
+  //   anime({
+  //     targets: [p1],
+  //     translateX: ["+100", "0"],
+  //     opacity: ["0%", "100%"],
+  //     easing: "easeInOutSine",
+  //     duration: 1000,
+  //     delay: delay,
+  //   });
+  // }
+  // $(".heading-first-ekam").on("inview", function (event, isInView) {
+  //   if (isInView) {
+  //     anime({
+  //       targets: [".heading-first-ekam", ".para-first-ekam"],
+  //       translateX: [-250, 0],
+  //       duration: 1000,
+  //       easing: "easeInOutSine",
+  //       opacity: ["0%", "100%"],
+  //       translateZ: 0,
+  //     });
+  //     leftToRight(".right-side-hero-img", 500);
+  //     anime({
+  //       targets: [".color-main"],
+  //       translateX: [+20, 0],
+  //       opacity: ["90%", "100%"],
+  //       easing: "easeInOutSine",
+  //       duration: 500,
+  //       translateZ: 0,
+  //     });
+  //   } else {
+  //     anime({
+  //       targets: [".heading-first-ekam", ".para-first-ekam"],
+  //       translateX: 0,
+  //       opacity: 0,
+  //     });
+  //     anime({
+  //       targets: [".right-side-hero-img"],
+  //       translateX: 0,
+  //       opacity: 0,
+  //     });
+  //   }
+  // });
+  // $(".color-features").on("inview", function (event, isInView) {
+  //   if (isInView) {
+  //     leftToRight(".heading-for-features-tab", 0);
+  //     anime({
+  //       targets: [".color-features"],
+  //       translateX: [-300, 0],
+  //       opacity: ["100%"],
+  //       easing: "easeInOutSine",
+  //       duration: 500,
+  //       translateZ: 0,
+  //     });
+  //   }
+  // });
 });
